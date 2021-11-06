f76d2157d88c957060e3112a5bf718f36b7037d0
jkaufy <jmk478@nau.edu>
2021-11-05 21:37:28 -0700

    this one is it!

--- tests/testthat/test-BINSEG.R
+++ tests/testthat/test-BINSEG.R
@@ -4,7 +4,7 @@ test_that("BINSEG output vector has reasonable size/values", {
   proifle_id <- "4"
   chromosome_num <- "2"
   nb.dt <- data.table(neuroblastoma$profiles)
-  data.dt <- nb.dt[nb.dt$profile.id == proifle_id & nb.dt$chromosome == chromosome_num]
+  data.dt <- nb.dt[nb.dt$profile.id == proifle_id & nb.dt$chromosome == chromosome_num,]
   num_segs <- 20
   my.binseg.result <- BINSEG(data.dt, num_segs)
   expect_equal(length(my.binseg.result), num_segs)
